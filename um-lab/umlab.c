e2a51c41e988cba0ab65e79048ffeec4e2e6305d
Henry J. Dotterer <hdotte01@vm-linux01.eecs.tufts.edu>
2026-03-27 20:17:47 -0400

    init

---
+++ um-lab/umlab.c
@@ -0,0 +1,137 @@
+/*
+ * umlab.c
+ *
+ * Functions to generate UM unit tests. Once complete, this module
+ * should be augmented and then linked against umlabwrite.c to produce
+ * a unit test writing program.
+ *  
+ * A unit test is a stream of UM instructions, represented as a Hanson
+ * Seq_T of 32-bit words adhering to the UM's instruction format.  
+ * 
+ * Any additional functions and unit tests written for the lab go
+ * here. 
+ *  
+ */
+
+
+#include <stdint.h>
+#include <stdio.h>
+#include <assert.h>
+#include <seq.h>
+#include <bitpack.h>
+#include <inttypes.h>
+
+
+typedef uint32_t Um_instruction;
+typedef enum Um_opcode {
+        CMOV = 0, SLOAD, SSTORE, ADD, MUL, DIV,
+        NAND, HALT, ACTIVATE, INACTIVATE, OUT, IN, LOADP, LV
+} Um_opcode;
+
+
+/* Functions that return the two instruction types */
+
+Um_instruction three_register(Um_opcode op, int ra, int rb, int rc);
+Um_instruction loadval(unsigned ra, unsigned val);
+
+
+/* Wrapper functions for each of the instructions */
+
+static inline Um_instruction halt(void) 
+{
+        return three_register(HALT, 0, 0, 0);
+}
+
+typedef enum Um_register { r0 = 0, r1, r2, r3, r4, r5, r6, r7 } Um_register;
+
+static inline Um_instruction add(Um_register a, Um_register b, Um_register c) 
+{
+        return three_register(ADD, a, b, c);
+}
+
+Um_instruction output(Um_register c);
+
+/* Functions for working with streams */
+
+static inline void append(Seq_T stream, Um_instruction inst)
+{
+        assert(sizeof(inst) <= sizeof(uintptr_t));
+        Seq_addhi(stream, (void *)(uintptr_t)inst);
+}
+
+const uint32_t Um_word_width = 32;
+
+void Um_write_sequence(FILE *output, Seq_T stream)
+{
+        assert(output != NULL && stream != NULL);
+        int stream_length = Seq_length(stream);
+        for (int i = 0; i < stream_length; i++) {
+                Um_instruction inst = (uintptr_t)Seq_remlo(stream);
+                for (int lsb = Um_word_width - 8; lsb >= 0; lsb -= 8) {
+                        fputc(Bitpack_getu(inst, 8, lsb), output);
+                }
+        }
+      
+}
+
+
+/* Unit tests for the UM */
+
+void build_halt_test(Seq_T stream)
+{
+        append(stream, halt());
+}
+
+void build_verbose_halt_test(Seq_T stream)
+{
+        append(stream, halt());
+        append(stream, loadval(r1, 'B'));
+        append(stream, output(r1));
+        append(stream, loadval(r1, 'a'));
+        append(stream, output(r1));
+        append(stream, loadval(r1, 'd'));
+        append(stream, output(r1));
+        append(stream, loadval(r1, '!'));
+        append(stream, output(r1));
+        append(stream, loadval(r1, '\n'));
+        append(stream, output(r1));
+}
+
+
+Um_instruction three_register(Um_opcode op, int ra, int rb, int rc)
+{
+        // create codeword
+        Um_instruction myInstruction = 0;
+        
+        // pack opcode into first 4 bits
+        myInstruction = (Um_instruction) op; 
+
+        // pad bits 27-9 with nothing (19 bits!!)
+        myInstruction = myInstruction << 19;
+        
+        int intArray[ ] = {ra, rb, rc};
+
+        for (int i = 0; i < 3; i++){
+                myInstruction = myInstruction << 3;
+                myInstruction = myInstruction | intArray[i];
+        }
+        printf("Codeword (decimal): %" PRIu32  "\n", myInstruction);
+        return myInstruction;
+}
+
+Um_instruction loadval(unsigned ra, unsigned val)
+{
+        // create codeword
+        Um_instruction myInstruction = 0;
+        
+        // pack opcode into first 4 bits
+        myInstruction = (Um_instruction)13;
+        myInstruction = myInstruction << 3;
+        myInstruction = myInstruction | ((Um_instruction)ra); 
+
+        // pad bits 27-9 with nothing
+        myInstruction = myInstruction << 25;
+        myInstruction = myInstruction | ((Um_instruction)val);
+
+        return myInstruction;
+}
