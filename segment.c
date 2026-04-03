a40ca0eb43cc1f8d44a53e351b2a335efd3470ca
Henry J. Dotterer <hdotte01@vm-linux01.eecs.tufts.edu>
2026-04-03 14:56:54 -0400

    add header file

--- segment.c
+++ segment.c
@@ -5,14 +5,6 @@
  *
  *  CS 40 Project 6: um
  *
- *  This module manages the UM’s segmented memory. It is responsible for 
- *  creating, freeing, and accessing memory segments, as well as reusing 
- *  unmapped segment IDs. Each segment is represented as a dynamically allocated
- *  array of 32-bit words together with its length. Segment 0 holds the 
- *  currently executing program, while all other mapped segments act as the UM’s
- *  general memory. This module provides the basic memory operations needed by 
- *  the emulator, including mapping a new segment, unmapping an existing 
- *  segment, loading a word from a segment, storing a word into a segment, and 
- *  duplicating a segment for the load-program instruction.
+ *  Implementation of the segment.h file
  * 
  */
