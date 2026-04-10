c7ebd9d001319161c82a8bf497aae312564c4c18
Henry J. Dotterer <hdotte01@vm-linux01.eecs.tufts.edu>
2026-04-09 20:04:32 -0400

    unmap and map checkpoint

--- instructions.c
+++ instructions.c
@@ -24,6 +24,10 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <iso646.h>
+
+#define EOF -1
+
 
 // all are helpers so all can be static
 static inline uint32_t get_opcode(uint32_t word);
@@ -57,16 +61,25 @@ bool run_instruction(UM_T um, uint32_t instruction)
         // check for op 13
         if (opcode = 13) {
                 // find register
+                uint32_t regA_13 = (instruction >> 25) & 0x7;
 
                 // find value
+                uint32_t val_13 = (instruction >> 25) & 0x01FFFFFF;
 
                 // put val in register
-        
-
+                um->reg[regA_13] = val_13;
 
                 return true;
         }
-        //set the registers vefore getting into the code
+
+
+        //set the registers vefore getting into the code: note use um->regs[reg]
+        // to access the actual value stored, i.e. what is below is the index
+        uint32_t ra = get_ra(instruction);
+        uint32_t rb = get_ra(instruction);
+        uint32_t rc = get_ra(instruction);
+
+
 
         // the rest of hte swtich cases
         switch (opcode) {
@@ -115,6 +128,8 @@ bool run_instruction(UM_T um, uint32_t instruction)
         return true;
 }
 
+
+// bit functions
 static inline uint32_t get_opcode(uint32_t word)
 {
         return word >> 28;
@@ -124,10 +139,104 @@ static inline uint32_t get_ra(uint32_t word)
 {
         return (word >> 6) & 0x7;
 }
+static inline uint32_t get_rb(uint32_t word)
+{
+        return (word >> 3) & 0x7;
+}
+static inline uint32_t get_rc(uint32_t word)
+{
+        return (word >> 0) & 0x7;
+}
 
+// command functions
 static void conditional_move(UM_T um, uint32_t ra, uint32_t rb, uint32_t rc)
 {
         if (um->regs[rc] != 0) {
                 um->regs[ra] = um->regs[rb];
         }
+}
+
+static void segmented_load(UM_T um, uint32_t ra, uint32_t rb, uint32_t rc)
+{
+        // um->segment[rb][]
+        um->regs[ra] = Segment_get(um->segments[um->regs[rb]], um->regs[rc]);
+}
+
+static void segmented_store(UM_T um, uint32_t ra, uint32_t rb, uint32_t rc)
+{
+        Segment_put(um->segments[um->regs[ra]], um->regs[rb], um->regs[rc]);
+}
+
+static void add(UM_T um, uint32_t ra, uint32_t rb, uint32_t rc)
+{
+        um->regs[ra] = (um->regs[rb] + um->regs[rc]);
+}
+
+static void multiply(UM_T um, uint32_t ra, uint32_t rb, uint32_t rc)
+{
+        um->regs[ra] = (um->regs[rb] * um->regs[rc]);
+}
+
+static void divide(UM_T um, uint32_t ra, uint32_t rb, uint32_t rc)
+{
+        um->regs[ra] = (um->regs[rb] / um->regs[rc]);
+}
+
+static void nand(UM_T um, uint32_t ra, uint32_t rb, uint32_t rc)
+{
+        um->regs[ra] = ~(um->regs[rb] & um->regs[rc])
+}
+
+static void map_segment(UM_T um, uint32_t rb, uint32_t rc)
+{
+        assert(um != NULL);
+
+        uint32_t length = um->regs[rc];
+        Segment_T new_seg = Segment_new(length);
+
+        uint32_t id;
+
+        if (Seq_length(um->unmapped) > 0) {
+                id = (uint32_t)(uintptr_t)Seq_remlo(um->unmapped);
+                um->segments[id] = new_seg;
+        } else {
+                // handle create new segment case
+
+        }
+
+        // set the value in rb to whatever id we end up with
+        um->regs[rb] = id;
+}
+
+static void unmap_segment(UM_T um, uint32_t rc)
+{
+        assert(um != NULL);
+
+        // set id equal to what is in rc
+
+        // free the segment at the id
+
+        // add the id to the sequence
+        Seq_addlo(um->unmapped, (void *)(uintptr_t)id);
+}
+
+static void output(UM_T um, uint32_t rc)
+{
+        uint32_t value = um->regs[rc];
+        putchar((char)value);
+}
+
+static void input(UM_T um, uint32_t rc)
+{
+        int32_t current = (int32_t)getchar();
+        if (current == EOF) {
+                um->regs[rc] = 0xffffffff;
+        } else {
+                um->regs[rc] = current % 256;
+        }
+}
+
+static void load_program(UM_T um, uint32_t rb, uint32_t rc)
+{
+
 }
