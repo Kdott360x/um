5427cf2dccef19f87a6c7712101328a0a7fd8d29
Henry J. Dotterer <hdotte01@j240l27.eecs.tufts.edu>
2026-04-21 17:50:29 -0400

    using calloc instead

--- segment.c
+++ segment.c
@@ -81,12 +81,8 @@ Segment_T Segment_new(uint32_t length)
         assert(seg != NULL);
 
         seg->length = length;
-        seg->words = malloc(length * sizeof(uint32_t));
+        seg->words = calloc(length, sizeof(uint32_t));
         assert(seg->words != NULL || length == 0);
-
-        for (uint32_t i = 0; i < length; i++) {
-                seg->words[i] = 0;
-        }
 
         return seg;
 }
